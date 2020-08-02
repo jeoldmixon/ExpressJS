8615821c552ad34a0559c2515a936f566455cae4
jeoldmixon <jeoldmixon@yahoo.com>
2020-08-01 21:03:21 -0500

    prettier

--- routes/apiRoute/noteTakerRoute.js
+++ routes/apiRoute/noteTakerRoute.js
@@ -1,32 +1,31 @@
 //Express
-const router = require('express').Router();
+const router = require("express").Router();
 // path
-const path = require('path');
+const path = require("path");
 //UUID
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 // db
-let notesArray = require('../../db/db.json');
+let notesArray = require("../../db/db.json");
 // to create or delete a note
-const { createNote, deleteNote, indexID } = require('../../lib/noteTaker');
+const { createNote, deleteNote, indexID } = require("../../lib/noteTaker");
 // fetch db
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../db/db.json'));
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../db/db.json"));
 });
 //delte note
-router.delete('/notes/:id', (req, res) => {
+router.delete("/notes/:id", (req, res) => {
     //index find and destroy
 
-    const note = deleteNote(req.params.id)
+    const note = deleteNote(req.params.id);
     res.json(note);
 });
 //need to push into notes array
-router.post('/notes', (req, res) => {
+router.post("/notes", (req, res) => {
     //UUID
     req.body.id = uuidv4();
     //create note
     const note = createNote(req.body);
-    res.json(note)
-
+    res.json(note);
 });
 //export
 module.exports = router;
--- server.js
+++ server.js
@@ -3,30 +3,32 @@
 // // path
 // const path = require('path');
 // express js
-const express = require('express');
+const express = require("express");
 // create server
 const app = express();
 //UUID  for id of array  // npm
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 // api routes
-const apiRoutes = require('./routes/apiRoute/noteTakerRoute');
+const apiRoutes = require("./routes/apiRoute/noteTakerRoute");
 // html routes
-const htmlRoutes = require('./routes/htmlRoute/htmlRoute');
+const htmlRoutes = require("./routes/htmlRoute/htmlRoute");
 // ports
 const PORT = process.env.PORT || 3001;
 //Public static folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 //parse incoming JSON
 app.use(express.json());
 //parse incoming string-array
 app.use(express.urlencoded({ extended: true }));
 //server to api
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 //server to html >> from Thursday lesson
-app.use('/', htmlRoutes);
+app.use("/", htmlRoutes);
 // server to allow use of files in public without explicit routes like zookeeper
-app.use(express.static('public'));
+app.use(express.static("public"));
 //local or Heroku port
 app.listen(PORT, () => {
-    console.log(`Server is now listening to port ${PORT}.  Unlike teenagers who listen to nothing.`);
+    console.log(
+        `Server is now listening to port ${PORT}.  Unlike teenagers who listen to nothing.`
+    );
 });
